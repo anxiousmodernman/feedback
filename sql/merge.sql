71ddef379206618dd46d0b2515278e869f40d463
Coleman McFarland <coleman.mcfarland@gmail.com>
2013-05-10 00:00:18 -0400

    a bit further on merge

---
+++ sql/merge.sql
@@ -0,0 +1,6 @@
+/*
+ * SQL file to test the merge function
+ */
+
+select * from link_subscriber_brief lsb
+where lsb.subscriberid = ''
--- tests.py
+++ tests.py
@@ -93,7 +93,7 @@ def getSubscriberId(**kwargs):
     cur.execute(sql)
     results = []  # will be list of
     for row in cur:
-        results.append(row['subscriberid'])
+        results.append(str(row['subscriberid']).upper())
     cur.close()
     test_conn.close()
     subscriberid = results.pop()
@@ -108,55 +108,17 @@ def id_generator(size=6, chars=string.ascii_uppercase + string.digits):
 
 def mergeRoutine(change_me, merge_into_me):
     sst.actions.start()
-    sst.actions.go_to('shark.smartbrief.com' + TOOLS_DIR + '/?email=' + change_me['email'])  # look up change_me
-
-    pass
-
-
-# def createWorkforceSubscriberViaWeb(**kwargs):
-#     sst.actions.start()
-#     sst.actions.go_to('https://www2.qa.smartbrief.com/aaaa')
-#     first_email_box = sst.actions.get_element_by_xpath('//*[@id="signupForm"]/div[5]/div[2]/input')
-#     confirm_email_box = sst.actions.get_element_by_xpath('//*[@id="signupForm"]/div[6]/div[2]/input')
-#     # use sst.actions function to 'type' into text box
-#     sst.actions.write_textfield(first_email_box, kwargs['email'])
-#     sst.actions.write_textfield(confirm_email_box, kwargs['email'])
-#     # get signup button element and click submit
-#     submit_button_page1 = sst.actions.get_element_by_css('#SignupDiv')
-#     sst.actions.click_element(submit_button_page1)
-#     # complete page 2: first get the elements
-#     sst.actions.sleep(10)
-#     first_name_field = sst.actions.get_element_by_xpath('//input[@name="firstName"]')
-#     last_name_field = sst.actions.get_element_by_xpath('//input[@name="lastName"]')
-#     company_field = sst.actions.get_element_by_xpath('//input[@name="company"]')
-#     title_field = sst.actions.get_element_by_xpath('//input[@name="title"]')
-#     zipcode_field = sst.actions.get_element_by_xpath('//input[@name="zipcode"]')
-#     country_field = sst.actions.get_element_by_xpath('//select[@name="country"]')
-#     position_level_field = sst.actions.get_element_by_xpath('//select[@name="positionLevel"]')
-#     position_function_field = sst.actions.get_element_by_xpath('(//*[@name="positionFunction"])[1]')  # surround w paren
-#     company_type_field = sst.actions.get_element_by_xpath('(//select[@name="companyType"])[1]')       # and add index[n]
-#     company_size_field = sst.actions.get_element_by_xpath('//select[@name="companySize"]')
-#     employees_field = sst.actions.get_element_by_xpath('//select[@name="employees"]')
-#     aaaa_custom_field_exists = sst.actions.exists_element(id="9C3D46B6-D106-49F5-9D01-661D9A5D566F")  # boolean
-#     submit_button_page2 = sst.actions.get_element_by_css("#SignupDiv")
-#     # complete page 2: now, write the fields
-#     sst.actions.write_textfield(first_name_field, kwargs['first_name'])
-#     sst.actions.write_textfield(last_name_field, kwargs['last_name'])
-#     sst.actions.write_textfield(company_field, kwargs['company'])
-#     sst.actions.write_textfield(title_field, kwargs['title'])
-#     sst.actions.write_textfield(zipcode_field, kwargs['zipcode'])
-#     sst.actions.set_dropdown_value(country_field, kwargs['country'])
-#     sst.actions.set_dropdown_value(position_level_field, "Staff")
-#     sst.actions.set_dropdown_value(position_function_field, "Sales")
-#     sst.actions.set_dropdown_value(company_type_field, "Agency: Full-service")
-#     sst.actions.set_dropdown_value(company_size_field, "N/A")
-#     sst.actions.set_dropdown_value(employees_field, "Less than 10")
-#     if aaaa_custom_field_exists:
-#         aaaa_custom_field = sst.actions.get_element_by_xpath('//select[@name="memberCategory"]')
-#         sst.actions.set_dropdown_value(aaaa_custom_field, "Not applicable")
-#         # Click submit button
-#     sst.actions.click_element(submit_button_page2)
-#     sst.actions.stop()
+    sst.actions.go_to('http://shark.smartbrief.com' + TOOLS_DIR + '/?email=' + change_me['email'])  # look up change_me
+    sst.actions.sleep(3)
+    edit_button = sst.actions.get_element_by_css('#editaccountshortprofile')
+    sst.actions.click_element(edit_button)
+    email_field = sst.actions.get_element_by_css('#emaileditshortprofile')
+    sst.actions.write_textfield(email_field, merge_into_me['email'])
+    sst.actions.click_element(edit_button)  # edit button doesn't change
+    sst.actions.accept_alert()
+    sst.actions.accept_alert()
+    sst.actions.sleep(1)
+    sst.actions.stop()
 
 
 class AlchemyConnectionTest(unittest.TestCase):
@@ -167,7 +129,7 @@ class AlchemyConnectionTest(unittest.TestCase):
         cur.execute('SELECT brief_name, full_brief_name from brief where briefid = %s', BRIEF_IDS['AAAA'])
         results = []  # will be list of
         for row in cur:
-            results.append(row['brief_name'])
+            results.append(str(row['brief_name']).upper())
         cur.close()
         test_conn.close()
         self.assertListEqual(results, ['AAAA'], "Connection to database failed")  # TODO better asserts case
@@ -197,7 +159,7 @@ class SubscriberTest(unittest.TestCase):
         'Save'. This test will fail of the subscriber is modified after the fact.
         """
         data = {
-            'email': 'save_subscriber_' + id_generator() + '@smartbrief.com',
+            'email': 'save.subscriber.' + id_generator() + '@smartbrief.com',
             'first_name': 'Save',
             'last_name': 'Subscriber',
             'title': 'Selenium Tester',
@@ -225,38 +187,59 @@ class SubscriberTest(unittest.TestCase):
         logging.info("BEGINNING MERGE TEST")
 
         change_this_subscriber = {
-            'email': 'change_this_subscriber_' + id_generator() + '@smartbrief.com',
+            'email': 'change.this.subscriber.' + id_generator() + '@smartbrief.com',
             'first_name': 'ChangeThis',
             'last_name': 'Subscriber',
-            'title': 'Changer',
+            'title': 'Giver',
             'city': 'Mexico City',
             'country': 'Mexico'
-            }
+        }
+
         change_this_subscriber = usergen.FeedbackSubscriber(**change_this_subscriber)
         change_this_subscriber = change_this_subscriber.__dict__
-        change_this_subscriber['subscriberid'] = getSubscriberId(**change_this_subscriber)  # set subscriberid
         createAAAASubscriberViaWeb(**change_this_subscriber)
+        #change_this_subscriber.__setattr__(self, 'subscriberid', getSubscriberId(**change_this_subscriber))
+        change_this_subscriber['subscriberid'] = getSubscriberId(**change_this_subscriber)  # set subscriberid
+
         logging.info("""[new subscriber] email = {email}\
                      """.format(email=change_this_subscriber['email']))
 
         merge_into_subscriber = {
-            'email': 'merge_into_subscriber_' + id_generator() + '@smartbrief.com',
-            'first_name': 'ChangeThis',
+            'email': 'merge.into.subscriber.' + id_generator() + '@smartbrief.com',
+            'first_name': 'MergeIntoThis',
             'last_name': 'Subscriber',
-            'title': 'Changer',
+            'title': 'Receiver',
             'city': 'Mexico City',
             'country': 'Mexico'
         }
+
         merge_into_subscriber = usergen.FeedbackSubscriber(**merge_into_subscriber)
         merge_into_subscriber = merge_into_subscriber.__dict__
-        merge_into_subscriber['subscriberid'] = getSubscriberId(**merge_into_subscriber)  # set subscriberid
         createCIASubscriberViaWeb(**merge_into_subscriber)
+        merge_into_subscriber['subscriberid'] = getSubscriberId(**merge_into_subscriber)  # set subscriberid
+        merge_into_subscriber
         logging.info("""[new subscriber] email = {email}\
                      """.format(email=merge_into_subscriber['email']))
         addSubscription(BRIEF_IDS['CIA10/8/2007'], **change_this_subscriber)
         addSubscription(BRIEF_IDS['AAAA10/3/2006'], **merge_into_subscriber)
         # Now that we have our subscriptions, do merge routine
         mergeRoutine(change_this_subscriber, merge_into_subscriber)
+        # Database checks
+        test_conn = AlchemyConnection()
+        cur = test_conn.getCursor()
+        sql = """select * from link_subscriber_brief lsb
+                 where lsb.subscriberid = '{email}' % subscriber['email']
+              """.format(email=change_this_subscriber['email'])
+        cur.execute(sql)
+        results = []  # will be list of
+        for row in cur:
+            results.append(row)
+        cur.close()
+        test_conn.close()
+
+        #logging.info("""[new subscriber] email = {email}\
+        #             """.format(email=subscriber['email']))
+
 
 
 
@@ -281,7 +264,7 @@ class SubscriberTest(unittest.TestCase):
         A merged into --> B
         
         *Final state*
-        Subscriber A: no active subscriptions, past subscriptions: AAAA, CIA/trial
+        Subscriber A: no active subscriptions, past subscriptions: AAAA, CIA/trial, CIA
         Subscriber B: active subscriptions: AAAA, CIA, past subscriptions: AAAA/trial
         """
 
